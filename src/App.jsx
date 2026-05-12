fac3661f18b446e0c7ece27801239b82f6b0c521
aastroid-destroyer <abraraqib33@gmail.com>
2026-05-13 00:53:38 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Comilla&appid=ee32ff8069054e425d1b4ae16ce08097&units=metric"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeatherData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-slate-900">
+        <span className="loading loading-spinner loading-lg text-white"></span>
+      </div>
+    );
+  }
 
   return (
     <></>
-  )
-}
+  );
+};
 
-export default App
+export default Weather;
